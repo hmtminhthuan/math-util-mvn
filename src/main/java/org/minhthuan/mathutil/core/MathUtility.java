056668f2829bc4ccfd7073343e8658da05d741e6
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2023-06-28 10:05:01 +0700

    first commit

---
+++ src/main/java/org/minhthuan/mathutil/core/MathUtility.java
@@ -0,0 +1,22 @@
+package org.minhthuan.mathutil.core;
+
+public class MathUtility {
+
+    public static final double PI = 3.1415;
+
+    public static long getFactorial(int n) {
+        
+        if (n < 0 || n > 20) 
+            throw new IllegalArgumentException("Invalid n. n must be between 0 and 20, please.");
+        
+        if (n == 0 || n == 1) 
+            return 1;
+        
+        long product = 1;
+        for (int i = 2; i <= n; i++)
+            product *= i;
+        
+        return product;
+//        return n * getFactorial(n - 1);
+    }
+}
---
+++ src/test/java/org/minhthuan/mathutil/core/test/MathUtilityAdvancedTest.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package org.minhthuan.mathutil.core.test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.minhthuan.mathutil.core.MathUtility;
+
+/**
+ *
+ * @author Minh Thuan
+ */
+public class MathUtilityAdvancedTest {
+
+    // => Chuẩn bị sẵn riếng data, lát fill vào => DDT
+    // Hàm trả về mang 2 chiều, kiểu wrapper class/object
+    // mảng chứa các cặp n -> expected
+    public static Object[][] initTestData() {
+        Object[][] testData = {{0, 1}, 
+                               {1, 1}, 
+                               {2, 2}, 
+                               {5, 120}, 
+                               {6, 720}
+                              };
+        return testData;
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "initTestData")
+    public void testFactorialGivenRightArgumentReturnsWell(int n, long expected) {
+        // DDT: DATA DRIVEN TESTING
+        // Tham số hóa việc đưa test data vào hàm assertE()
+        // biến đổi dữ liệu thành tham số cho tổng quát
+        // PARAMETERIZED
+        assertEquals(expected, MathUtility.getFactorial(n));
+    }
+
+}
